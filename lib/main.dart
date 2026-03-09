9254e59c725ff1610151e4a83df3df8c43f4d1dd
ismail <ismailcoder366@gmail.com>
2026-03-09 10:29:33 +0500

    title fetched from bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:bloc_tutorial/bloc/counter/counter_bloc.dart';
-import 'package:bloc_tutorial/equatable_testing.dart';
 import 'package:bloc_tutorial/repository/favourite_repository.dart';
-import 'package:bloc_tutorial/ui/counter_screen.dart';
 import 'package:bloc_tutorial/ui/favourate_app/favourate_app_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'bloc/favourate_app/favourate_app_bloc.dart';
 
 void main() {
@@ -15,7 +12,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -27,7 +24,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
               brightness: Brightness.dark
           ),
-          home: const FavourateAppScreen()
+          home:  FavourateAppScreen()
       ),
     );
   }
--- lib/ui/favourate_app/favourate_app_screen.dart
+++ lib/ui/favourate_app/favourate_app_screen.dart
@@ -1,13 +1,53 @@
+import 'package:bloc_tutorial/bloc/favourate_app/faourate_app_event.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class FavourateAppScreen extends StatelessWidget {
-  const FavourateAppScreen({super.key});
+import '../../bloc/favourate_app/favourate_app_bloc.dart';
+import '../../bloc/favourate_app/favourate_app_state.dart';
+
+class FavourateAppScreen extends StatefulWidget {
+  @override
+  State<FavourateAppScreen> createState() => _FavourateAppScreenState();
+}
+
+class _FavourateAppScreenState extends State<FavourateAppScreen> {
+  @override
+
+  void initState() {
+    super.initState();
+    context.read<FavourateAppBloc>().add(FetchFavouriteList());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Favourate App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: BlocBuilder<FavourateAppBloc, FavourateAppState>(
+            builder: (context, state){
+              switch(state.listStatus){
+                case ListStatus.loading:
+                  return Center(child: CircularProgressIndicator());
+                case ListStatus.failure:
+                  return Text('error');
+                  case ListStatus.success:
+                return ListView.builder(
+                    itemCount: state.favouriteItemList.length,
+                    itemBuilder: (context, index){
+                      final item = state.favouriteItemList[index];
+                      return Card(
+                        child: ListTile(
+                          title: Text(item.value.toString()),
+
+                        ),
+                      );
+                    });
+              }
+
+        }),
       ),
     );
   }
